31bb7a0abd454d471d1b404ab47226cfde86720b
Shekar Kasani <shekarkasani002@gmail.com>
2025-06-01 02:51:26 +0530

    Update backend to listen on dynamic port

--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,10 @@ app.use('/api/notes', notesRouter);
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 .then(() => {
   console.log('Connected to MongoDB');
-  app.listen(5000, () => console.log('Server running on http://localhost:5000'));
-})
+ const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
 .catch(err => console.error(err));
